aba68dc753d4f9e34481487cea387759d07e7f93
Samuel <stotino2000@yahoo.com>
2020-10-10 14:06:55 -0700

    display list of directories and files

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-
-$files = scandir('../data');
-
-echo '<ul>';
-foreach ($files as $file){
-    echo '<li>';
-    echo $file;
-    echo '</li>';
+function readFolderFiles($dir){
+    $files = scandir($dir);
+    //echo var_dump($files);
+    //echo '<pre>';
+    //print_r($files);
+    //echo '</pre>';
+    
+    echo '<ul>';
+    foreach ($files as $file){
+        if (substr($file, 0, 1) != '.' || $file == 'index.php') {
+            echo '<li>';
+            if(is_dir($dir . '/' . $file)){
+                echo $file;
+                readFolderFiles($dir . '/' . $file);
+            }else{
+                echo '<a href="' . $dir . '/' . $file . '">' . $file . '</a>';
+            }
+            echo '</li>';
+        }
+    }
+    echo '</ul>';
 }
-echo '</ul>';
+
+readFolderFiles('../data');
